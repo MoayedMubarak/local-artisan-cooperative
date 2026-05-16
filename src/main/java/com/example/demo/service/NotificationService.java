9a833494ced7067579e2a5fe7e0d410946f46d4f
Abo7asan <alihassan200666@gmail.com>
2026-05-17 00:02:29 +0300

    Revert "test"
    This reverts commit 34f8db31fbf1916031618bfb3f7d9e9ffbcdcb62.

--- src/main/java/com/example/demo/service/NotificationService.java
+++ src/main/java/com/example/demo/service/NotificationService.java
@@ -5,8 +5,6 @@ import com.example.demo.model.User;
 import com.example.demo.repository.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,7 +14,6 @@ public class NotificationService {
     @Autowired
     private NotificationRepository notificationRepository;
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void sendNotification(User user, String title, String message, String type, String link) {
         Notification notification = new Notification(user, title, message, type, link);
         notificationRepository.save(notification);
